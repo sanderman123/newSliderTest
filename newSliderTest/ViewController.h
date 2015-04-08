f5195ac3aa25b26033bd882aac415cffb07f7338
Sander Valstar <sanderman123@hotmail.com>
2015-04-08 18:12:09 +0800

    Initial Commit

---
+++ newSliderTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newSliderTest
+//
+//  Created by Sander Valstar on 4/8/15.
+//  Copyright (c) 2015 Sander Valstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ newSliderTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  newSliderTest
+//
+//  Created by Sander Valstar on 4/8/15.
+//  Copyright (c) 2015 Sander Valstar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
