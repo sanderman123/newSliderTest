c9027455144ede234d66961cbd6eda4d819ec48b
Sander Valstar <sanderman123@hotmail.com>
2015-04-08 19:18:43 +0800

    first commit

--- newSliderTest/ViewController.m
+++ newSliderTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "EFCircularSlider.h"
 
 @interface ViewController ()
 
@@ -17,11 +18,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect sliderFrame = CGRectMake(110, 150, 75, 75);
+    EFCircularSlider* circularSlider = [[EFCircularSlider alloc] initWithFrame:sliderFrame];
+    [self.view addSubview:circularSlider];
+    [circularSlider addTarget:self action:@selector(roundSliderChanged:) forControlEvents:UIControlEventValueChanged];
+    circularSlider.lineWidth = 4;
+
+    circularSlider.handleType = EFDoubleCircleWithOpenCenter;
+    circularSlider.filledColor = [UIColor colorWithRed:(float)55/255 green:(float)152/255 blue:(float)255/255 alpha:1.0f];
+    circularSlider.handleColor = circularSlider.filledColor;
+
+    circularSlider.unfilledColor = [UIColor colorWithRed:(float)182/255 green:(float)182/255 blue:(float)182/255 alpha:1.0f];
+    
+    circularSlider.minimumValue = 0.0f;
+    circularSlider.maximumValue = 2.0f;
+    circularSlider.currentValue = 1.0f;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)roundSliderChanged:(id)sender {
+    
 }
 
 @end
